1e885df5e32cc1c73967037d9e362d8f09981571
zhangzhiqiang <zhangzhiqiang2@vipkid.com.cn>
2020-06-05 14:20:07 +0800

    直接内存溢出。

--- src/com/zzq/OutOfMemory.java
+++ src/com/zzq/OutOfMemory.java
@@ -1,6 +1,9 @@
 package com.zzq;
 
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -12,8 +15,8 @@ public class OutOfMemory {
      * 使用如下参数运行JVM即会产生。
      * java.lang.OutOfMemoryError: Java heap space
      * JVM参数：
-     * -Xms5m -Xmx5m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./jvm.dump
-     * 使用-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./jvm.dump
+     * -Xms5m -Xmx5m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./jvm.hprof
+     * 使用-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./jvm.hprof
      * 可以使代码在发生 java.lang.OutOfMemoryError: Java heap space时生成一份内存快照用于分析堆溢出的原因。
      *
      */
@@ -37,7 +40,7 @@ public class OutOfMemory {
      * java.lang.OutOfMemoryError:GC over head limit exceeded .此方法并不会抛出此异常。
      * 这个异常很难复现。但是本方法可以产生一个频繁full gc的log。
      * gc了但是必须效果并不理想，可以使用次参数。此异常并不会中断当前线程
-     * -Xms3m -Xmx3m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./jvm.dump -Xloggc:./gc.log
+     * -Xms3m -Xmx3m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./jvm.hprof -Xloggc:./gc.log
      */
     public static void gcNotEffect() {
         Executor executor = new ThreadPoolExecutor(2, 2, 10000L, TimeUnit.MINUTES, new LinkedBlockingQueue<>());
@@ -61,10 +64,24 @@ public class OutOfMemory {
 
     }
 
+    /**
+     * Direct buffer memory堆外内存溢出。常见场景 使用NIO包下内容。
+     *-Xms3m -Xmx3m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./jvm.dump -Xloggc:./gc.log -XX:MaxDirectMemorySize=1m -verbose:gc -XX:+PrintGCDetails
+     */
+    public static void directMemory() {
+        List<ByteBuffer> list = new ArrayList<ByteBuffer>();
+        while (true) {
+            ByteBuffer buffer = ByteBuffer.allocateDirect(1 * 1024 * 1024);
+            list.add(buffer);
+        }
+    }
+
+
     public static void main(String[] args) {
-        // heapSpace();
+        //  heapSpace();
         //unableToCreateThread();
-        gcNotEffect();
+        // gcNotEffect();
+        directMemory();
     }
 
 
