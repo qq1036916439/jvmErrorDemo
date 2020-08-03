a2b1579a7c24ead39a2167ab0ab729f67cf039e1
zhangzhiqiang <zhangzhiqiang2@vipkid.com.cn>
2020-08-03 19:17:05 +0800

    33-search

---
+++ src/com/zzq/leetcode/search.java
@@ -0,0 +1,36 @@
+package com.zzq.leetcode;
+
+/**
+ * @author zhangzhiqiang
+ */
+public class search {
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[left] <= nums[mid]) {
+                if ((nums[left] <= target && target < nums[mid])) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        search(new int[]{4, 5, 6, 7, 0, 1, 2}, 3);
+    }
+}
