9a942abc95d74ce4d08f7965a3c2e1368dcc2947
zhangzhiqiang <zhangzhiqiang2@vipkid.com.cn>
2020-08-05 20:29:56 +0800

    328-奇偶链表先提交下debug出问题了。。。

---
+++ src/com/zzq/leetcode/oddEvenList.java
@@ -0,0 +1,36 @@
+package com.zzq.leetcode;
+
+import com.zzq.listNode.ListNode;
+
+import static com.zzq.listNode.ListNodeFunction.arrayToListNode;
+import static com.zzq.listNode.ListNodeFunction.printListNode;
+
+/**
+ * @author zhangzhiqiang
+ */
+public class oddEvenList {
+    public static ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return head;
+        }
+        ListNode node1 = head;
+        ListNode node2 = head.next;
+        ListNode head2 = node2;
+        node1.next = node2.next;
+        while (node1 != null && node2 != null) {
+            node1.next = node2.next;
+            node1 = node1.next;
+            node2.next = node1.next;
+            node2 = node2.next;
+        }
+        node1.next = head2;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = arrayToListNode(1, 2, 3, 4, 5, 6, 7,8);
+        oddEvenList(head);
+        printListNode(head);
+    }
+
+}
--- src/com/zzq/listNode/ListNodeFunction.java
+++ src/com/zzq/listNode/ListNodeFunction.java
@@ -5,14 +5,26 @@ package com.zzq.listNode;
  */
 public class ListNodeFunction {
 
-    public static ListNode arrayToListNode(int...val){
+    public static ListNode arrayToListNode(int... val) {
         ListNode head = new ListNode(0);
-        ListNode curr=head;
+        ListNode curr = head;
         for (int i : val) {
-            curr.next=new ListNode(i);
-            curr=curr.next;
+            curr.next = new ListNode(i);
+            curr = curr.next;
         }
         return head.next;
+    }
+
+    /**
+     * 按照顺序输出链表
+     * @param node
+     * @return
+     */
+    public static void printListNode(ListNode node) {
+        while (node != null) {
+            System.out.println(node.val);
+            node = node.next;
+        }
     }
 
 
