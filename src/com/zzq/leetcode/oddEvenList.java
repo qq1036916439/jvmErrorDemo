f04dd29e9f56716d517e0096d203d7137ba4db2f
zhangzhiqiang <zhangzhiqiang2@vipkid.com.cn>
2020-08-06 19:31:30 +0800

    328-奇偶链表:优化下优雅的代码

--- src/com/zzq/leetcode/oddEvenList.java
+++ src/com/zzq/leetcode/oddEvenList.java
@@ -13,23 +13,44 @@ public class oddEvenList {
         if (head == null || head.next == null || head.next.next == null) {
             return head;
         }
-        ListNode node1 = head;
-        ListNode node2 = head.next;
-        ListNode head2 = node2;
-        node1.next = node2.next;
-        while (node1 != null && node2 != null) {
-            node1.next = node2.next;
-            if (node1.next == null) {
-                break;
-            }
-            node1 = node1.next;
-            node2.next = node1.next;
-            node2 = node2.next;
+        ListNode o = head;
+        ListNode e = head.next;
+        ListNode p = e;
+        while (o != null && e != null) {
+            o.next = e.next;
+            o = o.next;
+            o.next = o.next;
+            e = e.next;
         }
-        node1.next = head2;
+        e.next = p;
         return head;
     }
 
+    /**
+     * public:
+     *     ListNode* oddEvenList(ListNode* head) {
+     *         if(!head||!head->next||!head->next->next) return head;
+     *         // 指向奇链表结尾
+     *         ListNode* o = head;
+     *         // 指向偶链表结尾
+     *         ListNode* e = head->next;
+     *         // 指向偶链表开头
+     *         ListNode* p = e;
+     *
+     *         // 将奇偶节点分别加入到对应的链表中
+     *         while(o->next&&e->next){
+     *             o->next = e->next;
+     *             o = o->next;
+     *             e->next = o->next;
+     *             e = e->next;
+     *         }
+     *
+     *         // 拼接两个链表
+     *         o->next = p;
+     *         return head;
+     *     }
+     * };
+     */
     public static void main(String[] args) {
         ListNode head = arrayToListNode(1, 2, 3, 4, 5, 6, 7, 8);
         oddEvenList(head);
